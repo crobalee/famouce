2bdf9fba93d17871ab9ee990057492eb7bd327c8
Educational Account Manager <my1226@naver.com>
2025-08-13 23:37:28 +0900

    Fix TypeScript build errors for Vercel deployment

--- src/components/FamousPersonSearch.tsx
+++ src/components/FamousPersonSearch.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { searchFamousPerson } from '../services/famousPersonService';
 import './FamousPersonSearch.css';
 
